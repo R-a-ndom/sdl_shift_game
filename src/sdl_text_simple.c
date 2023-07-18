73bd8e854e5a2f721a978c29909e52d41710b5bc
R1a1ndom <alxy778@gmail.com>
2023-07-19 02:51:55 +0300

    Adding SDL_TEXT_SIMPLE.H/.C file - simple text output

---
+++ src/sdl_text_simple.c
@@ -0,0 +1,15 @@
+/* * * * * * * * * * * * * * * * * * * *
+ *                                     *
+ *  << SDL 2.0 hand-made extention >>  *
+ *                                     *
+ *  SDL_TEXT_SIMPLE.C -                *
+ *     simple text output              *
+ *     16 x 8 cells letters (SDL 2.0)  *
+ *                                     *
+ *         (c) Alexey Sorokin, 2023... *
+ *                                     *
+ * * * * * * * * * * * * * * * * * * * */
+
+
+
+#include "sdl_text_simple.h"
---
+++ src/sdl_text_simple.h
@@ -0,0 +1,19 @@
+/* * * * * * * * * * * * * * * * * * * *
+ *                                     *
+ *  << SDL 2.0 hand-made extention >>  *
+ *                                     *
+ *  SDL_TEXT_SIMPLE.H -                *
+ *     simple text output              *
+ *     16 x 8 cells letters (SDL 2.0)  *
+ *                                     *
+ *         (c) Alexey Sorokin, 2023... *
+ *                                     *
+ * * * * * * * * * * * * * * * * * * * */
+
+
+#ifndef SDL_TEXT_SIMPLE_H
+#define SDL_TEXT_SIMPLE_H
+
+
+
+#endif
